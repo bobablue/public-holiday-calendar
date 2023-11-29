231879aca6430173892cc92acb4872934cbaf5a6
Junjie Huang <junjiehuang@Junjies-MacBook-Air.local>
2023-11-29 12:00:12 +0800

    Updated to fetch 2024 holidays

--- get_public_holidays.py
+++ get_public_holidays.py
@@ -1,36 +1,45 @@
+"""fetch public holiodays of selected countries from API and save as flat file."""
 import requests
 import pandas as pd
 
-#%% static data
-countries = {'AU':'Australia','CN':'China','DE':'Germany','HK':'Hong Kong',
-             'ID':'Indonesia','IN':'India','JP':'Japan','KR':'South Korea',
-             'MY':'Malaysia','NZ':'New Zealand','PH':'Philippines','LK':'Sri Lanka',
-             'SG':'Singapore','TH':'Thailand','TW':'Taiwan','GB':'United Kingdom',
-             'VN':'Vietnam'}
+#%% static data (https://date.nager.at/)
+meta = {'url':'https://date.nager.at/api/v3/PublicHolidays/{year}/{country}',
+        'year':'2024',
+        'data_keep':['date','name','localName','countryCode'],
+        'countries':{'AU':'Australia', 'CN':'China', 'DE':'Germany', 'HK':'Hong Kong',
+                     'ID':'Indonesia', 'IN':'India', 'JP':'Japan', 'KR':'South Korea',
+                     'MY':'Malaysia', 'NZ':'New Zealand', 'PH':'Philippines', 'LK':'Sri Lanka',
+                     'SG':'Singapore', 'TH':'Thailand', 'TW':'Taiwan', 'GB':'United Kingdom',
+                     'VN':'Vietnam'}}
 
-#%% url and params (https://date.nager.at/)
-url_base = 'https://date.nager.at/api/v3/PublicHolidays/2023/country'
+meta['url'] = meta['url'].replace('{year}', meta['year'])
 
 #%% get data
 hols = {}
-for code, cty in countries.items():
-    response = requests.get(url=url_base.replace('country',code))
+for code, cty in meta['countries'].items():
+    response = requests.get(url=meta['url'].replace('{country}',code))
     if response.content:
         hols[cty] = response.json()
+
+        # keep only selected json data
+        for i in enumerate(hols[cty]):
+            hols[cty][i[0]] = {k:v for k,v in hols[cty][i[0]].items() if k in meta['data_keep']}
+
     else:
         print(f"[Not available] {cty}")
 
 #%% pass json data to dataframe
 hols_df = pd.DataFrame()
-for cty in hols.keys():
+for cty in list(hols):
     hols_df = pd.concat([hols_df, pd.DataFrame(hols[cty])])
 
 #%% tidy up, keep only tidied columns (date, subject, description)
 hols_df['Date'] = pd.to_datetime(hols_df['date']).dt.date
-hols_df['Subject'] = '[' + hols_df['countryCode'].map(countries) + '] ' + hols_df['name']
+hols_df['Subject'] = '[' + hols_df['countryCode'].map(meta['countries']) + '] ' + hols_df['name']
 hols_df = hols_df.rename(columns={'localName':'Description'})
 
 hols_df = hols_df[['Date','Subject','Description']]
+hols_df = hols_df.drop_duplicates().reset_index(drop=True)
 
 #%% export
-hols_df.to_csv('Public Holidays.csv', encoding='utf-16', index=False)
+hols_df.to_csv(f"Public Holidays_{meta['year']}.csv", encoding='utf-16', index=False)
